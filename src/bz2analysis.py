9433d1596f530d986aa65d6ee1fb6abd225581f5
ehrenb <branden.a.ehrenreich@gmail.com>
2022-12-26 11:21:31 -0500

    Initial commit

---
+++ src/bz2analysis.py
@@ -0,0 +1,32 @@
+import base64
+import bz2
+import json
+
+from machina.core.worker import Worker
+
+class Bz2Analysis(Worker):
+    types = ['bz2']
+    next_queues = ['Identifier']
+
+    def __init__(self, *args, **kwargs):
+        super(Bz2Analysis, self).__init__(*args, **kwargs)
+
+    def callback(self, data, properties):
+        data = json.loads(data)
+
+        # resolve path
+        target = self.get_binary_path(data['ts'], data['hashes']['md5'])
+        self.logger.info(f"resolved path: {target}")
+
+        with bz2.open(target, 'rb') as f:
+            data_encoded = base64.b64encode(f.read()).decode()
+            body = {
+                "data": data_encoded,
+                "origin": {
+                    "ts": data['ts'],
+                    "md5": data['hashes']['md5'],
+                    "id": data['id'], #I think this is the only field needed, we can grab the unique node based on id alone
+                    "type": data['type']
+                }
+            }
+            self.publish_next(json.dumps(body))
---
+++ src/run.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from bz2analysis import Bz2Analysis
+
+d = Bz2Analysis()
+d.start_consuming()
