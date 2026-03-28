a023683ef785fb7cc21947158867463a67338fcc
ehrenb <branden.a.ehrenreich@gmail.com>
2026-03-28 18:39:24 -0400

    update get_binary_path() to include incoming type

--- src/bz2analysis.py
+++ src/bz2analysis.py
@@ -15,7 +15,7 @@ class Bz2Analysis(Worker):
         data = json.loads(data)
 
         # resolve path
-        target = self.get_binary_path(data['ts'], data['hashes']['md5'])
+        target = self.get_binary_path(data['ts'], data['hashes']['md5'], data['type'])
         self.logger.info(f"resolved path: {target}")
 
         with bz2.open(target, 'rb') as f:
